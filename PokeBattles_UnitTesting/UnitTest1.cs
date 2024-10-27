b481ba8ace031402ede90461623691e55d7b95fd
DaanF1 <daanstander@hotmail.nl>
2024-10-27 15:41:43 +0100

    Added Unit Test project

---
+++ PokeBattles_UnitTesting/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Avans_PokeBattles;
+using Avans_PokeBattles.Server;
+
+namespace PokeBattles_UnitTesting
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Lobby _lobby = new Lobby();
+        }
+    }
+}
